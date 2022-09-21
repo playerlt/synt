cbd7750aa40eb884156153a6d071e97a78c3a02d
player <3127321495@qq.com>
2022-09-21 17:48:02 +0800

    同步传

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"os/signal"
+
+	"github.com/playerlt/synt/config"
+	"github.com/playerlt/synt/server"
+)
+
+func main() {
+	chChromeDie := make(chan struct{})
+	chBackendDie := make(chan struct{})
+	chSignal := listenToInterrupt()
+	go server.Run()
+	//go startBrowser(chChromeDie, chBackendDie)
+	for {
+		select {
+		case <-chSignal:
+			chBackendDie <- struct{}{}
+		case <-chChromeDie:
+			os.Exit(0)
+		}
+	}
+}
+
+func startBrowser(chChromeDie chan struct{}, chBackendDie chan struct{}) {
+	// 先写死路径，后面再照着 lorca 改
+	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+config.GetPort()+"/static/index.html")
+	cmd.Start()
+	go func() {
+		<-chBackendDie
+		cmd.Process.Kill()
+	}()
+	go func() {
+		cmd.Wait()
+		chChromeDie <- struct{}{}
+	}()
+}
+
+func listenToInterrupt() chan os.Signal {
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt)
+	return chSignal
+}
---
+++ server/server.go
@@ -0,0 +1,53 @@
+package server
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/playerlt/synt/config"
+	c "github.com/playerlt/synt/server/controller"
+	"github.com/playerlt/synt/server/ws"
+)
+
+//go:embed frontend/dist/*
+var FS embed.FS
+
+func Run() {
+	hub := ws.NewHub()
+	go hub.Run()
+
+	gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+	staticFiles, _ := fs.Sub(FS, "frontend/dist")
+	router.POST("/api/v1/files", c.FilesController)
+	router.GET("/api/v1/qrcodes", c.QrcodesController)
+	router.GET("/uploads/:path", c.UploadsController)
+	router.GET("/api/v1/addresses", c.AddressesController)
+	router.POST("/api/v1/texts", c.TextsController)
+	router.GET("/ws", func(c *gin.Context) {
+		ws.HttpController(c, hub)
+	})
+	router.StaticFS("/static", http.FS(staticFiles))
+	router.NoRoute(func(c *gin.Context) {
+		path := c.Request.URL.Path
+		if strings.HasPrefix(path, "/static/") {
+			reader, err := staticFiles.Open("index.html")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer reader.Close()
+			stat, err := reader.Stat()
+			if err != nil {
+				log.Fatal(err)
+			}
+			c.DataFromReader(http.StatusOK, stat.Size(), "text/html;charset=utf-8", reader, nil)
+		} else {
+			c.Status(http.StatusNotFound)
+		}
+	})
+	router.Run(":" + config.GetPort())
+}
